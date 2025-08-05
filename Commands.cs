7ebb89c974c17b261b9405c3c58a6eace0d47e55
Aaron Shackelford | LG240762 <aaron.shackelford@wateraic.com>
2025-08-05 15:17:30 -0700

    First Hello World, Pages 1-9 of the handout.

---
+++ Commands.cs
@@ -0,0 +1,36 @@
+﻿// Example based off the guide found here: https://static.au-uw2-prd.autodesk.com/Class_Handout_SD467609_Ben_Rand.pdf
+
+using Autodesk.AutoCAD.Runtime; // CommandMethod attribute comes from this namespace
+using Autodesk.AutoCAD.ApplicationServices; // missing from example, added for Document
+using System;
+using System.Linq;
+namespace Parcels
+{
+    public class Commands
+    {
+        /**
+        When the user types PS_Hello in the AutoCAD command line,
+        the following method `Hello()` will be executed.
+
+        This attribute comes from the Autodesk.AutoCAD.Runtime
+        */
+        [CommandMethod("PS_Hello")]
+        public void Hello()
+        {
+            // var document = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            //      Making the document variable explicit in the following uncommented line.
+
+            Document document = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            // Document type comes from the Autodesk.AutoCAD.ApplicationServices namespace
+            //      MdiActiveDocument is the currently active document in AutoCAD.
+
+            var editor = document.Editor;
+            // Editor is a property of the Document class that provides access to the command line and other editor features.
+            //      The Editor can do things like select objects in drawings, write messages, etc.
+
+            editor.WriteMessage("\nHello World!");
+            // Writes "Hello World!" to the command line in AutoCAD.
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Parcels
+
+This is a project based off the Parsels AutoDesk Plugin Class.
+
+The project instructions can be found here:
+
+- [Create Your First AutoCAD Plug-In by Ben Rand](https://www.autodesk.com/autodesk-university/class/Create-Your-First-AutoCAD-Plug-2020?msockid=330dfa63d9e16669310defb5d8bf671a)
+
+This project specifically follows the handout found here:
+
+- [Class_Handout_SD467609_Ben_Rand.pdf [Download]](https://static.au-uw2-prd.autodesk.com/Class_Handout_SD467609_Ben_Rand.pdf)
+
+_Also found in the repo file_
+
+## Requirements:
+
+- AutoCAD - any non free version
+- Visual Studio 2019+ (community version or enterprise)
+
+## Instructions:
+
+Follow along with the PDF.
